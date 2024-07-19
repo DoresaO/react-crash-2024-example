62c74702b6de4b2ae168492ab06c07e6e0a71e8d
DoresaO <doresa.osmanaj@student.uni-pr.edu>
2024-07-19 15:58:45 +0200

    Init

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card=({Children, bg='bg-gray-100'})=>{
+    return(
+        <div className={`${bg} p-6 rounded-lg shadow-md`}>
+            {Children}
+        </div>
+    )
+}
+export default Card
